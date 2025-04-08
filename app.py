153a7047f206d50a9bc650046f39c48c47d5b08b
shovonex <82492667+shovonex@users.noreply.github.com>
2025-04-08 01:55:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from flask import Flask, request
-from bot import main
+from telegram_bot.bot import main
 import os
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    """Handle incoming updates from Telegram"""
+    """Handle incoming Telegram updates"""
     update = request.get_json()
     main(update)
     return 'OK', 200
 
-def set_webhook():
-    from telegram import Bot
-    bot = Bot(token=os.getenv('TELEGRAM_TOKEN'))
-    bot.set_webhook(url='https://russ-bot.onrender.com/webhook')
-
 if __name__ == '__main__':
-    set_webhook()
     app.run(host='0.0.0.0', port=8000)
